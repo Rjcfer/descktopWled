4e9d8bf9367a37a160b23b62ffdd9bd00aa9cd61
Ricardo <Correia.ferreira.ricardo@gmail.com>
2023-06-01 16:35:06 +0200

    work

--- view.js
+++ view.js
@@ -5,7 +5,10 @@ $(function () {
 $(document).ready(function () {
   $("#countbtn").click(function () {
     $.ajax({
-      url: "http://api.meteo-concept.com/api/location/city?token=ad6fae89aeb81a84549f56a17667035c2060e05b9684ca5d3562cd4b8c259639&insee=51230",
+      url:
+        "http://api.meteo-concept.com/api/location/city?token=" +
+        config.token_meteo +
+        "&insee=51230",
       type: "get",
       success: function (data) {
         console.log(data);
