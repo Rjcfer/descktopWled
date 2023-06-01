cf2cbe653df819546327c6c0c691a5743a2ad325
Ricardo <Correia.ferreira.ricardo@gmail.com>
2023-06-01 19:24:34 +0200

    work

--- view.js
+++ view.js
@@ -25,6 +25,114 @@ $(() => {
   $("#countbtn").on("click", () => {
     count++;
     $("#click-counter").text(count);
-    console.log(config.token_meteo);
+    postData(data);
   });
 });
+
+async function postData(data = {}) {
+  $.ajax({
+    type: "POST",
+    url: "192.168.1.42/json/state",
+    data: JSON.stringify({ Markers: markers }),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function (data) {
+      alert(data);
+    },
+    error: function (errMsg) {
+      alert(errMsg);
+    },
+  });
+}
+
+let data = {
+  on: true,
+  bri: 128,
+  transition: 7,
+  mainseg: 0,
+  seg: [
+    {
+      id: 0,
+      start: 0,
+      stop: 32,
+      startY: 0,
+      stopY: 8,
+      grp: 1,
+      spc: 0,
+      of: 0,
+      on: true,
+      frz: false,
+      bri: 23,
+      cct: 127,
+      n: "TOTO",
+      col: [
+        [0, 0, 255],
+        [0, 0, 0],
+        [0, 0, 0],
+      ],
+      fx: 122,
+      sx: 128,
+      ix: 128,
+      pal: 43,
+      c1: 0,
+      c2: 128,
+      c3: 16,
+      sel: true,
+      rev: false,
+      mi: false,
+      rY: false,
+      mY: false,
+      tp: false,
+      o1: false,
+      o2: false,
+      o3: false,
+      si: 0,
+      m12: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+    {
+      stop: 0,
+    },
+  ],
+};
