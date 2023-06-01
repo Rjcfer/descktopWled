dba0aafe6e4c02693a3f3ba4f6f9ddc9a84962de
Ricardo <Correia.ferreira.ricardo@gmail.com>
2023-06-01 10:07:34 +0200

    work

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+// creer une fenetre au demarrage de l'application lors que l'application est prete et la promesse est resolue
+app.whenReady().then(() => {
+  createWindow();
+  // quitter des que touts les fenetres sont fermes sauf sur mac
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+  });
+});
