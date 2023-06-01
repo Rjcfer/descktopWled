13f38bf5d147bf6111c996d6f75621930005fb02
Ricardo <Correia.ferreira.ricardo@gmail.com>
2023-06-01 15:07:57 +0200

    work

---
+++ src/view.js
@@ -0,0 +1,7 @@
+const $ = require("jquery"); // jQuery now loaded and assigned to $
+let count = 0;
+$("#click-counter").text(count.toString());
+$("#countbtn").on("click", () => {
+  count++;
+  $("#click-counter").text(count);
+});
